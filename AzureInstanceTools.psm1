4e71bceaeb500938f3e2b242dd05646c1e477805
lremedi <lremedi@gmail.com>
2014-10-23 12:10:24 -0300

    Adding new function to run remote scripts in an Azure VM

--- AzureInstanceTools.psm1
+++ AzureInstanceTools.psm1
@@ -30,5 +30,43 @@ param(
     }
     $instances
 }
+function Invoke-RmtAzure {
+    Param(
+        [string]$vm_username,
+        [string]$vm_password,
+        [string]$vm_name,
+        [string]$azure_service_name,
+        [string]$scriptPath
+    )
+    write-Host ("Configuring credentials...")
 
-Export-ModuleMember -Function Stop-AllAzureInstances, Get-AzureInstancesWithName
+    $winRMCert = (Get-AzureVM -ServiceName $azure_service_name -Name $vm_name | select -ExpandProperty vm).DefaultWinRMCertificateThumbprint
+
+    $azureX509cert = Get-AzureCertificate -ServiceName $azure_service_name -Thumbprint $winRMCert -ThumbprintAlgorithm sha1
+
+    $certTempFile = [IO.Path]::GetTempFileName()
+    $azureX509cert.Data | Out-File $certTempFile
+
+    $certToImport = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $certTempFile
+
+    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store "Root", "LocalMachine"
+    $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+    $store.Add($certToImport)
+    $store.Close()
+
+    Remove-Item $certTempFile
+
+    $uri = Get-AzureWinRMUri -ServiceName $azure_service_name -Name $vm_name
+
+    $secpwd = ConvertTo-SecureString $vm_password -AsPlainText -Force
+    $credential = New-Object System.Management.Automation.PSCredential($vm_username, $secpwd)
+    $sessionopt = New-PSSessionOption -SkipCNCheck -SkipCACheck
+
+    write-Host "Executing $scriptPath in $vm_name..."
+    Invoke-Command `
+    -ConnectionUri $uri.ToString() `
+    -Credential $credential `
+    -SessionOption $sessionopt `
+    -FilePath $scriptPath
+}
+Export-ModuleMember -Function Stop-AllAzureInstances, Get-AzureInstancesWithName, Invoke-RmtAzure
